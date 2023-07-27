3aab52f2e5c39f783463d6c1890ea747c084f9d9
Fırat Degirmencioğlu <degirmencioglu.frt@gmail.com>
2023-07-27 07:30:14 +0300

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Header from './component/Header';
+import Post from './component/post';
+import Layout from './component/Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import { UserContextProvider } from './component/UserContext';
+import CreatePost from './pages/CreatePost';
+import PostPage from './pages/PostPage';
+import EditPost from './pages/EditPost';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <UserContextProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<IndexPage />} />
+            <Route path={'/login'} element={<LoginPage />} />
+            <Route path={'/register'} element={<RegisterPage />} />
+            <Route path={'/create'} element={<CreatePost />} />
+            <Route path="/post/:id" element={<PostPage />} />
+            <Route path='/edit/:id' element={<EditPost />} />
+          </Route>
+        </Routes>
+      </UserContextProvider>
+    </BrowserRouter>
+
+
+  );
+}
+
+export default App;
